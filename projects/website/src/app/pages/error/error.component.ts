d4aead45046d6fb692170ddbf531ebfd9ca1877b
Gabe LaPoint <glapoint22@gmail.com>
2022-01-30 10:27:14 -0500

    Updated side menus

--- projects/website/src/app/pages/error/error.component.ts
+++ projects/website/src/app/pages/error/error.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LazyLoadingService } from '../../services/lazy-loading/lazy-loading.service';
 
 @Component({
   selector: 'error',
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.scss']
 })
-export class ErrorComponent { }
+export class ErrorComponent implements OnInit {
+
+  constructor(private lazyLoadingService: LazyLoadingService) { }
+
+  ngOnInit(): void {
+    this.lazyLoadingService.container.clear();
+  }
+}
