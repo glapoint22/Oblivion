ac2247739128dbd9d4b18a84eebfb99ea86cfe1b
Gabe LaPoint <glapoint22@gmail.com>
2022-02-10 15:12:11 -0500

    Added components folder

---
+++ projects/manager/src/app/components/builder/builder.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BuilderComponent } from './builder.component';
+
+
+
+@NgModule({
+  declarations: [BuilderComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BuilderModule { }
