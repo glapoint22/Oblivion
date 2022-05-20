45e738a32f1a3ffc64e2edde18abdf94adcb381e
Gabe LaPoint <glapoint22@gmail.com>
2022-05-20 15:01:45 -0400

    Worked on product slider

--- projects/manager/src/app/components/product-slider-widget-dev/product-slider-widget-dev.component.ts
+++ projects/manager/src/app/components/product-slider-widget-dev/product-slider-widget-dev.component.ts
@@ -8,5 +8,13 @@ import { WidgetService } from '../../services/widget/widget.service';
   styleUrls: ['./product-slider-widget-dev.component.scss']
 })
 export class ProductSliderWidgetDevComponent extends ProductSliderWidgetComponent {
+
   constructor(public widgetService: WidgetService) { super() }
+
+  ngOnInit(): void {
+    super.ngOnInit();
+    this.caption.text = 'Check out these products!';
+    this.caption.fontSize = this.caption.fontSizes[8];
+    this.caption.color = '#ffba00';
+  }
 }
