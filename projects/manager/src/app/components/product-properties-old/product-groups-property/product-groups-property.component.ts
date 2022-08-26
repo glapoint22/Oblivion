06588e1e6775ca6fad71dcc3eebcd4af4d5e1d72
Gabe LaPoint <glapoint22@gmail.com>
2022-08-26 12:13:36 -0400

    Did some cleanup

--- projects/manager/src/app/components/product-properties-old/product-groups-property/product-groups-property.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'product-groups-property',
-  templateUrl: './product-groups-property.component.html',
-  styleUrls: ['./product-groups-property.component.scss']
-})
-export class ProductGroupsPropertyComponent { }
